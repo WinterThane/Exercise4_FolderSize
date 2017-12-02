0cd67ed9dead0605418a53a4e54f9fd68c6feec4
WinterThane <demonsaso@gmail.com>
2017-12-02 16:30:16 +0100

    Final commit
    App show size of a folder

--- Exercise4_FolderSize/Exercise4_FolderSize/Program.cs
+++ Exercise4_FolderSize/Exercise4_FolderSize/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise4_FolderSize
 {
@@ -10,6 +8,25 @@ namespace Exercise4_FolderSize
     {
         static void Main(string[] args)
         {
+            var dirInfo = new DirectoryInfo(@"C:\Tutorials");
+            long directorySize = DirectorySize(dirInfo, true);
+
+            Console.WriteLine("Directory size in B:  {0:N0} Bytes", directorySize);
+            Console.WriteLine("Directory size in KB: {0:N2} KB", ((double)directorySize) / 1024);
+            Console.WriteLine("Directory size in MB: {0:N2} MB", ((double)directorySize) / (1024 * 1024));
+            Console.ReadLine();
+        }
+
+        private static long DirectorySize(DirectoryInfo dirInfo, bool includeSubDir)
+        {
+            long size = dirInfo.EnumerateFiles().Sum(file => file.Length);
+
+            if(includeSubDir)
+            {
+                size += dirInfo.EnumerateDirectories().Sum(dir => DirectorySize(dir, true));
+            }
+
+            return size;
         }
     }
 }
